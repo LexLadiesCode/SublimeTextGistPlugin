50183869e488d89dd63b4624db1270872f7300c8
Sarah Vessels <cheshire137@gmail.com>
2015-05-14 18:41:53 -0400

    Get the selected text
    Fixes #2

--- gist-creator.py
+++ gist-creator.py
@@ -4,6 +4,7 @@ class GistCreatorCommand(sublime_plugin.TextCommand):
   def run(self, edit, cmd=None):
     # Insert text in the file the user is editing, on the line where their cursor is
     for region in self.view.sel():
-      line = self.view.line(region)
-      line_contents = self.view.substr(line) + "\n"
-      self.view.insert(edit, line.begin(), "hello world")
+      if not region.empty():
+        gist_content = self.view.substr(region)
+        line = self.view.line(region)
+        self.view.insert(edit, line.begin(), gist_content)
