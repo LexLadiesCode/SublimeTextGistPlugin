5cd812e7d1373c90fd8b79be2b9a89f023e9876a
Sarah Vessels <cheshire137@gmail.com>
2015-05-14 17:51:56 -0400

    there => their
    The shame…

--- gist-creator.py
+++ gist-creator.py
@@ -2,7 +2,7 @@ import sublime, sublime_plugin
 
 class GistCreatorCommand(sublime_plugin.TextCommand):
   def run(self, edit, cmd=None):
-    # Insert text in the file the user is editing, on the line where there cursor is
+    # Insert text in the file the user is editing, on the line where their cursor is
     for region in self.view.sel():
       line = self.view.line(region)
       line_contents = self.view.substr(line) + "\n"
